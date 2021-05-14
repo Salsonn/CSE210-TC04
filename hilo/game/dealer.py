7402f2c977b8ce0fcadcb9b00556a8247f79be8f
Caleb Salyards <34790708+Psymatics@users.noreply.github.com>
2021-05-14 17:30:03 -0600

    Attempting to piece things together...

---
+++ hilo/game/dealer.py
@@ -0,0 +1,70 @@
+
+
+import random
+
+list = {
+    1: ['A ♣', 'A ♥', 'A ♦', 'A ♠'],
+    2: ['2 ♣', '2 ♥', '2 ♦', '2 ♠'],
+    3: ['3 ♣', '3 ♥', '3 ♦', '3 ♠'],
+    4: ['4 ♣', '4 ♥', '4 ♦', '4 ♠'],
+    5: ['5 ♣', '5 ♥', '5 ♦', '5 ♠'],
+    6: ['6 ♣', '6 ♥', '6 ♦', '6 ♠'],
+    7: ['7 ♣', '7 ♥', '7 ♦', '7 ♠'],
+    8: ['8 ♣', '8 ♥', '8 ♦', '8 ♠'],
+    9: ['9 ♣', '9 ♥', '9 ♦', '9 ♠'],
+    10: ['10 ♣', '10 ♥', '10 ♦', '10 ♠'],
+    11: ['J ♣', 'J ♥', 'J ♦', 'J ♠'],
+    12: ['Q ♣', 'Q ♥', 'Q ♦', 'Q ♠'],
+    13: ['K ♣', 'K ♥', 'K ♦', 'K ♠']
+}
+
+class Dealer:
+
+    global list
+
+    def __init__(self):
+        self.cards = list
+        self.score = 0
+
+    def random_card(self):
+
+        # Chooses a random card from self.cards
+        self.given_card = random.choice(list[random.randint(1, 13)])
+
+        # Finds the value of the found card
+        self.card_value(self.given_card)
+
+        # Removes selected card from the deck
+        self.remove_card(self.given_card)
+
+        # If this feat. is somehow attained, this will only trigger
+        # If the player gets all 52 cards correct somehow
+        if self.cards == []:
+            # Returns a string if all cards correctly guessed
+            print('No more cards left. You Win!')
+            quit()
+        else:
+            # Otherwise, it just returns the card and its value
+            return self.given_card
+    
+    def remove_card(self, given_card):
+        # Iterates through each value in the list
+        for i in list:
+
+            # This checks if the card was found
+            if given_card in list[i]:
+
+                # If the card was found, it grabs its index value and pops it off the list
+                index_value = list[i].index(given_card)
+                list[i].pop(index_value)
+
+    def card_value(self, given_card):
+        
+        # Iterates through the list to find the given card
+        for i in list:
+            # Once the card is found, it finds the card value and returns it
+            if given_card in list[i]:
+                print(i)
+                return i
+        print("Error: Unable to recall card value.")
+        return 0
--- hilo/game/director.py
+++ hilo/game/director.py
@@ -4,6 +4,72 @@ class Director:
     # The Director behaves as the person overseeing the game, commands the dealer,
     # gets user input, keeps score, and determines when the game is over.
     def __init__(self):
+<<<<<<< Updated upstream
         # Starts the Dealer and prepares the game.
 
         self.dealer = Dealer()
+=======
+        '''Setting up the appropriate variables and initializing dealer'''
+        self.dealer = Dealer()
+        self.keep_playing = True
+
+    def get_input(self):
+        '''Display the starting card for each deal. 
+        Then receive input from user'''
+        self.display_chosen()
+        self.player_guess = input('High or Low [h/l]: ')
+
+    def do_updates(self):
+        '''Update the score and quit if score is 0'''
+        '''if self.dealer.check_guess(self.player_guess) == True:
+            self.dealer.score += 100
+        elif self.dealer.check_guess(self.player_guess) == False:
+            self.dealer.score -= 75'''
+        # print(f"comparing card 1 ({self.first_card}")
+        # print(f"value is: {self.dealer.card_value(self.first_card)}")
+        # print(f"With card 2 ({self.second_card}")
+        if self.dealer.card_value(self.first_card) < self.dealer.card_value(self.second_card):
+            if self.player_guess == "h":
+                self.dealer.score += 100
+            else:
+                self.dealer.score -= 75
+        if self.dealer.card_value(self.first_card) > self.dealer.card_value(self.second_card):
+            if self.player_guess == "l":
+                self.dealer.score += 100
+            else:
+                self.dealer.score -= 75
+
+    def do_outputs(self):
+        '''Displays the next unknown card
+        and prompt the user if they want to 
+        keep plaing the game'''
+        self.display_future()
+        print("Your score is now " . self.dealer.score)
+
+        if self.dealer.score > 0:
+            play_again = input(f'Would you like to play again? [y/n]')
+            if play_again == 'n':
+                self.keep_playing = False
+        else:
+            print("Game Over!")
+            quit()
+
+    def start_game(self):
+        '''Game loop'''
+        while self.keep_playing:
+            self.first_card = self.dealer.random_card()
+            self.get_input()
+            self.second_card = self.dealer.random_card()
+            self.do_updates()
+            self.do_outputs()
+
+    def display_chosen(self):
+        '''Displays the first card'''
+        print(f'The first card card is: {self.first_card}')
+        print("It's value is:")
+        self.dealer.card_value(self.first_card)
+
+    def display_future(self):
+        '''Reveals the next card'''
+        print(f'The card is: {self.second_card}')
+>>>>>>> Stashed changes
