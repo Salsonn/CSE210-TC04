fa31aee4ea142e98f6a7452372a20035fc93f48e
Josh Thieme <jomthieme@gmail.com>
2021-05-13 18:07:34 -0600

    Added comments to methods

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -3,16 +3,20 @@ from game.dealer import Dealer
 class Director:
 
     def __init__(self):
+        '''Setting up the appropriate variables'''
         self.keep_playing = True
-       # self.chosen_card = self.dealer.random_card()
+        self.chosen_card = self.dealer.random_card()
         self.future_card = self.chosen_card
         self.dealer = Dealer()
 
     def get_input(self):
+        '''Display the starting card for each deal. 
+        Then receive input from user'''
         self.display_chosen()
         self.player_guess = input('High or Low [h/l]: ')
 
     def do_updates(self):
+        '''Update the score and quit if score is 0'''
         if self.dealer.check_guess(self.player_guess) == True:
             self.dealer.score += 100
         elif self.dealer.check_guess(self.player_guess) == False:
@@ -21,6 +25,9 @@ class Director:
             quit()
 
     def do_outputs(self):
+        '''Displays the next unknown card
+        and prompt the user if they want to 
+        keep plaing the game'''
         self.display_future()
 
         if self.dealer.score > 0:
@@ -33,15 +40,18 @@ class Director:
             self.keep_playing = False
 
     def start_game(self):
+        '''Game loop'''
         while self.keep_playing:
             self.get_input()
             self.do_updates()
             self.do_outputs()
 
     def display_chosen(self):
+        '''Displays the chosen card'''
         print(f'the chosen card is: {self.chosen_card}')
 
     def display_future(self):
+        '''Reveals the next card'''
         print(f'the future card is: {self.future_card}')
         
 
